508cfe32ab8d634d4dd662a7c20aabf975ebabd7
nuxsmin <nuxsmin@cygnux.org>
2016-05-27 02:08:11 +0200

    Closes #9 Related #8

--- inc/SMD/Util/Util.class.php
+++ inc/SMD/Util/Util.class.php
@@ -130,7 +130,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $build = 2016050201;
+        $build = 2016052701;
         $version = array(1, 0);
 
         if ($retBuild) {
