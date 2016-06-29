deb9530ebe577d020f79550eecbb0e5486ce62dc
nuxsmin <nuxsmin@cygnux.org>
2016-06-29 13:29:55 +0200

    * [MOD] Updated version&changelog

--- inc/SMD/Util/Util.class.php
+++ inc/SMD/Util/Util.class.php
@@ -130,7 +130,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $build = 2016061601;
+        $build = 2016062401;
         $version = array(1, 0);
 
         if ($retBuild) {
